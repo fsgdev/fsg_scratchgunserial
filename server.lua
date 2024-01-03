bc554b360e4cbd7b3ac556d48eb4fc810fe114b8
fsg <101289888+fsgdev@users.noreply.github.com>
2024-01-03 01:43:53 +0000

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,9 +4,15 @@ lib.addCommand('scratchgunserial', {
     help = 'Scratch the current gun\'s serial'
 }, function(source, args, raw)
     local weapon = inv:GetCurrentWeapon(source)
-    weapon.metadata.serial = 'The serial has been scratched off.'
-    inv:SetMetadata(source, weapon.slot, weapon.metadata)
-    TriggerClientEvent("chat:addMessage", source, {
-        template = '<div style="color: rgba(255, 99, 71, 1); width: fit-content; max-width: 300%; word-break: break-word; "> ^4[INFO]^0 '..weapon.label..': The serial has been removed </div>',
-    })
+    if weapon ~= nil then
+        weapon.metadata.serial = 'The serial has been scratched off.'
+        inv:SetMetadata(source, weapon.slot, weapon.metadata)
+        TriggerClientEvent("chat:addMessage", source, {
+            template = '<div style="color: rgba(255, 99, 71, 1); width: fit-content; max-width: 300%; word-break: break-word; "> ^4[INFO]^0 '..weapon.label..': The serial has been removed </div>',
+        })
+    else
+        TriggerClientEvent("chat:addMessage", source, {
+            template = '<div style="color: rgba(255, 99, 71, 1); width: fit-content; max-width: 300%; word-break: break-word; "> ^4[INFO]^0 You do not have a weapon to scratch the serial from. </div>',
+        })
+    end
 end)
